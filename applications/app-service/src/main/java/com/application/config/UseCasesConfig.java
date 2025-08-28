ccfb892cdf044c6d866da5460bda3adb6bad23c4
Harold21Mtz <hfmartinezc@ufpso.edu.co>
2025-08-28 09:46:18 -0500

    Initialize project structure with Gradle configuration, Dockerfile, .gitignore, and base resources.

---
+++ applications/app-service/src/main/java/com/application/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.application.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.application.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
